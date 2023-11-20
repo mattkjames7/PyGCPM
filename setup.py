c2c092ae39092861c1a57fc0f799115cfd1742ff
matt <mattkjames7@gmail.com>
2023-11-20 23:02:19 +0000

    added code to make source dist, hopefully

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
-import setuptools
+from setuptools import setup, find_packages
 from setuptools.command.install import install
 import os
+import platform
+from setuptools.command.build_py import build_py
+import subprocess
+
+
+class CustomBuild(build_py):
+    def run(self):
+        self.execute(self.target_build, ())
+        build_py.run(self)
+
+    def target_build(self):
+        if platform.system() == 'Windows':
+            cwd = os.getcwd()
+            os.chdir('PyGCPM/__data/libgcpm/')
+            subprocess.check_call(['cmd','/c','compile.bat'])
+            os.chdir(cwd)
+        else:
+            subprocess.check_call(['make', '-C', 'PyGCPM/__data/libgcpm'])
+
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -30,7 +50,7 @@ def getversion():
 	
 version = getversion()
 
-setuptools.setup(
+setup(
     name="PyGCPM",
     version=version,
     author="Matthew Knight James",
@@ -39,7 +59,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mattkjames7/PyGCPM",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+    package_data={'testmodule2': ['**/*']},
+    cmdclass={'build_py': CustomBuild},  
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License (GPL)",
